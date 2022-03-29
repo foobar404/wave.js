9a001b0db0fe7d5695c9d3be6b4cb857a222d6bb
Austin Michaud λ <austinthemichaud@gmail.com>
2022-03-28 17:25:44 -0700

    ci-cd update

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+        exclude: /node_modules/
+      }
     ],
   },
   resolve: {
