47da46e22a5b2962f61342597018603e6a6ea01a
mvishu405 <36050287+mvishu405@users.noreply.github.com>
2022-12-15 11:52:32 +0530

    made the prod build

---
+++ dist/src/types/index.d.ts
@@ -0,0 +1,45 @@
+export declare type FillOption = string | {
+    gradient: string[];
+    rotate?: number;
+} | {
+    image: string;
+};
+export declare type Glow = {
+    strength: number;
+    color: string;
+};
+export interface IArcOptions {
+    glow?: Glow;
+    lineColor?: FillOption;
+    lineWidth?: number;
+    rounded?: boolean;
+}
+export interface ICircleOptions {
+    fillColor?: FillOption;
+    glow?: Glow;
+    lineColor?: FillOption;
+    lineWidth?: number;
+}
+export interface ILineOptions {
+    glow?: Glow;
+    lineColor?: FillOption;
+    lineWidth?: number;
+    rounded?: boolean;
+}
+export interface IPolygonOptions {
+    fillColor?: FillOption;
+    glow?: Glow;
+    lineColor?: FillOption;
+    lineWidth?: number;
+    rounded?: boolean;
+}
+export interface IRectangleOptions {
+    glow?: Glow;
+    fillColor?: FillOption;
+    lineColor?: FillOption;
+    lineWidth?: number;
+    radius?: number;
+}
+export interface IAnimation {
+    draw: (audioBufferData: Uint8Array, canvasElement: CanvasRenderingContext2D) => void;
+}
---
+++ dist/website/src/pages/Home/index.d.ts
@@ -0,0 +1,1 @@
+export declare function Home(): JSX.Element;
